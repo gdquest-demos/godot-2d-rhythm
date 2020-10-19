d0abc1fa2bf7f4870c937804a492d63d35b2baed
Francois Belair <razoric480@gmail.com>
2020-10-19 11:29:57 -0400

    Draw circle with a shader instead of draw_arc

--- godot/RhythmGame/Hits/TargetCircle.gd
+++ godot/RhythmGame/Hits/TargetCircle.gd
@@ -1,22 +1,31 @@
 extends Position2D
 
+const THICKNESS := 0.015
+
 export var shrink_speed := 0.0
 
 var radius := 0.0
 var end_radius := 0.0
 var fill_color := Colors.WHITE
+var max_radius := 0.0
 
 onready var animation_player := $AnimationPlayer
+onready var target_circle := $TargetCircle
 
 
 func set_up(radius_start: float, radius_end: float, bps: float, beat_delay: float) -> void:
 	radius = radius_start
 	end_radius = radius_end
+	max_radius = radius
 	shrink_speed = 1.0 / bps / beat_delay * (radius - end_radius)
 
+	target_circle.margin_left = -radius
+	target_circle.margin_right = radius
+	target_circle.margin_top = -radius
+	target_circle.margin_bottom = radius
 
-func _draw() -> void:
-	draw_arc(Vector2.ZERO, radius, 0.0, 2 * PI, 100, fill_color, 6.0, true)
+	target_circle.rect_size = Vector2.ONE * radius * 2
+	target_circle.material = target_circle.material.duplicate()
 
 
 func _process(delta: float) -> void:
@@ -24,4 +33,4 @@ func _process(delta: float) -> void:
 	if radius <= end_radius and shrink_speed > 0.0:
 		radius = end_radius
 		animation_player.play("destroy")
-	update()
+	target_circle.material.set_shader_param("torus_radius", radius / max_radius / 2)
