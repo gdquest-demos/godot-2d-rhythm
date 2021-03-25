db22242f86542b5729d4ef7a282a48bd06e8f461
Nathan Lovato <nathan@gdquest.com>
2021-03-25 13:29:18 -0600

    Simplify DragDetector

--- godot/RhythmGame/UI/TrackSelector/DragDetector.gd
+++ godot/RhythmGame/UI/TrackSelector/DragDetector.gd
@@ -1,35 +1,11 @@
 extends Area2D
-class_name Dragger
+class_name DragDetector
 
 signal dragged(amount)
 
 export var strength := 2.0
 
-var _dragging := false
-var _start_position := Vector2()
-
-
-func _start_detection(position: Vector2) -> void:
-	_dragging = true
-	_start_position = position
-
-
-func _end_detection(position: Vector2) -> void:
-	var amount: Vector2 = position - _start_position  #.normalized()
-	_start_position = position
-	emit_signal("dragged", amount * strength)
-
 
 func _on_input_event(_viewport, event, _shape_idx) -> void:
-	if event.is_action_pressed("touch"):
-		_start_detection(event.position)
-
-	if event.is_action_released("touch"):
-		_dragging = false
-
-	if _dragging and event is InputEventMouseMotion:
-		_end_detection(event.position)
-
-
-func _on_mouse_exited() -> void:
-	_dragging = false
+	if event is InputEventMouseMotion and Input.is_action_pressed("touch"):
+		emit_signal("dragged", event.relative * strength)
